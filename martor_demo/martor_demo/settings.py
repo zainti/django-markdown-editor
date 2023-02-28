0fc36cf5d010cc7ef3a8c291b61f731474675c60
Zainti <zainti@gmail.com>
2023-02-28 13:44:33 +0100

    add unit test and static files

--- martor_demo/martor_demo/settings.py
+++ martor_demo/martor_demo/settings.py
@@ -145,3 +145,6 @@ STATIC_URL = '/static/'
 MEDIA_URL = '/media/'
 STATIC_ROOT = os.path.join(tempfile.gettempdir(), 'martor_static')
 MEDIA_ROOT = os.path.join(tempfile.gettempdir(), 'martor_media')
+
+STATIC_ROOT = os.path.join(BASE_DIR, "static")
+MEDIA_ROOT = os.path.join(BASE_DIR, "media")
